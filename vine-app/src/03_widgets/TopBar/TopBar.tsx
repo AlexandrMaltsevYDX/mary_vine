a2d3c39aee94e9eb07c24d636c53919cf6109c78
Aleksandr Bakshtaev <abakshtaev@aquivalabs.com>
2023-11-02 21:26:50 +0100

    TF-4: finished

--- vine-app/src/03_widgets/TopBar/TopBar.tsx
+++ vine-app/src/03_widgets/TopBar/TopBar.tsx
@@ -8,20 +8,31 @@ import Box from "@mui/material/Box";
 import ButtonGroup from "@mui/material/ButtonGroup";
 import Button from "@mui/material/Button";
 import { Avatar } from "@mui/material";
+import SignInModalWindow from "~widgets/SignInModalWindow/SignInModalWindow";
 // tollar провайдит цвет фона
 const TopBar: React.FC = () => {
+  const [isSignInModalOpen, setIsSignInModalOpen] = useState(false);
+
+  const onOpenListener = () => {
+    setIsSignInModalOpen(!isSignInModalOpen);
+  }
+
+
   return (
-    <Toolbar
-      disableGutters
-      sx={{ padding: "0", bgcolor: "primary.dark", height: "100%", justifyContent: "space-between", paddingRight: "15px" }}
-    >
-      <SideBarToggleButton />
-      <ButtonGroup variant="contained" aria-label="outlined primary button group">
-        <Button>Sign In</Button>
-        <Button>Sign Up</Button>
-        <Avatar alt="Remy Sharp" src="images/sample-avatar.jpg" />
-      </ButtonGroup>
-    </Toolbar>
+    <>
+      <Toolbar
+        disableGutters
+        sx={{ padding: "0", bgcolor: "primary.dark", height: "100%", justifyContent: "space-between", paddingRight: "15px" }}
+      >
+        <SideBarToggleButton />
+        <ButtonGroup variant="contained" aria-label="outlined primary button group">
+          <Button onClick={() => onOpenListener()}>Sign In</Button>
+          <Button>Sign Up</Button>
+          <Avatar alt="Remy Sharp" src="images/sample-avatar.jpg" />
+        </ButtonGroup>
+        <SignInModalWindow open={isSignInModalOpen} handleClose={() => onOpenListener()} />
+      </Toolbar>
+    </>
   );
 };
 
