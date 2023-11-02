62ea80c66d67a4624b4dca4eb840564616e6d557
Aleksandr Bakshtaev <abakshtaev@aquivalabs.com>
2023-11-02 21:04:23 +0100

    TF-10: finished

--- vine-app/src/03_widgets/TopBar/TopBar.tsx
+++ vine-app/src/03_widgets/TopBar/TopBar.tsx
@@ -5,15 +5,22 @@ import MenuIcon from "@mui/icons-material/Menu";
 import Toolbar from "@mui/material/Toolbar";
 import SideBarToggleButton from "~widgets/SideBarToggleButton/SideBarToggleButton";
 import Box from "@mui/material/Box";
-
+import ButtonGroup from "@mui/material/ButtonGroup";
+import Button from "@mui/material/Button";
+import { Avatar } from "@mui/material";
 // tollar провайдит цвет фона
 const TopBar: React.FC = () => {
   return (
     <Toolbar
       disableGutters
-      sx={{ padding: "0", bgcolor: "primary.dark", height: "100%" }}
+      sx={{ padding: "0", bgcolor: "primary.dark", height: "100%", justifyContent: "space-between", paddingRight: "15px" }}
     >
       <SideBarToggleButton />
+      <ButtonGroup variant="contained" aria-label="outlined primary button group">
+        <Button>Sign In</Button>
+        <Button>Sign Up</Button>
+        <Avatar alt="Remy Sharp" src="images/sample-avatar.jpg" />
+      </ButtonGroup>
     </Toolbar>
   );
 };
