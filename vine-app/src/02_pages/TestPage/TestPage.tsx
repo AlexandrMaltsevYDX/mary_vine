287a055b671ce727ddff82634950a5322455214a
AlexandrMaltsevYDX <109986802+AlexandrMaltsevYDX@users.noreply.github.com>
2023-11-05 19:02:04 +0100

    list

--- vine-app/src/02_pages/TestPage/TestPage.tsx
+++ vine-app/src/02_pages/TestPage/TestPage.tsx
@@ -1,9 +1,5 @@
 import React from "react";
-import styles from "./TestPage.module.scss";
-import MyCard from "~shared/MyCard/MyCard";
 import Crmpage from "~shared/CrmPage/Crmpage";
-import ContentBlock from "~shared/ContentBlock/ContentBlock";
-import CommonInfoContentBlock from "~pages/MyProfilePage/components/ui/CommonInfoContentBlock/CommonInfoContentBlock";
 import DataTable from "~shared/ListBase/ListBase";
 
 // js hello world
